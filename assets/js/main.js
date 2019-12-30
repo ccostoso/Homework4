48710ecb324d5e73be9a33348d151ac92d08caa2
ccostoso <carlos.costoso@outlook.com>
2019-12-30 18:47:43 -0500

    Restored import to main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,96 +3,96 @@ var isPlaying;
 var isCharSelected;
 var charArr = [];
 
-// import Character from './character';
+import Character from './character';
 
 // // Create character objects
-class Character {
-    constructor(name, HP, AP, CAP, imageSrc) {
-        this._name = name;
-        this._HP = HP;
-        this._baseHP = HP;
-        this._AP = AP;
-        this._baseAP = AP;
-        this._CAP = CAP;
-        this._baseCAP = CAP;
-        this._imageSrc = imageSrc;
-    }
+// class Character {
+//     constructor(name, HP, AP, CAP, imageSrc) {
+//         this._name = name;
+//         this._HP = HP;
+//         this._baseHP = HP;
+//         this._AP = AP;
+//         this._baseAP = AP;
+//         this._CAP = CAP;
+//         this._baseCAP = CAP;
+//         this._imageSrc = imageSrc;
+//     }
 
-    get name() {
-        return this._name;
-    }
+//     get name() {
+//         return this._name;
+//     }
 
-    set name(newName) {
-        this._name = newName;
-    }
+//     set name(newName) {
+//         this._name = newName;
+//     }
 
-    get HP() {
-        return this._HP;
-    }
+//     get HP() {
+//         return this._HP;
+//     }
 
-    set HP(newHP) {
-        this._HP = newHP;
-    }
+//     set HP(newHP) {
+//         this._HP = newHP;
+//     }
 
-    get baseHP() {
-        return this._baseHP;
-    }
+//     get baseHP() {
+//         return this._baseHP;
+//     }
 
-    get AP() {
-        return this._AP;
-    }
+//     get AP() {
+//         return this._AP;
+//     }
 
-    set AP(newAP) {
-        this._AP = newAP;
-    }
+//     set AP(newAP) {
+//         this._AP = newAP;
+//     }
 
-    get baseAP() {
-        return this._baseAP;
-    }
+//     get baseAP() {
+//         return this._baseAP;
+//     }
 
-    get CAP() {
-        return this._CAP;
-    }
+//     get CAP() {
+//         return this._CAP;
+//     }
 
-    set CAP(newCAP) {
-        this._CAP = newCAP;
-    }
+//     set CAP(newCAP) {
+//         this._CAP = newCAP;
+//     }
 
-    get baseCAP() {
-        return this._baseCAP;
-    }
+//     get baseCAP() {
+//         return this._baseCAP;
+//     }
 
-    get imageSrc() {
-        return this._imageSrc;
-    }
+//     get imageSrc() {
+//         return this._imageSrc;
+//     }
 
-    set imageSrc(newImageSrc) {
-        this._imageSrc = newImageSrc;
-    }
+//     set imageSrc(newImageSrc) {
+//         this._imageSrc = newImageSrc;
+//     }
 
-    pushInCharArr() {
-        charArr.push(this);
-    }
+//     pushInCharArr() {
+//         charArr.push(this);
+//     }
 
-    boostAP() {
-        this.AP = Math.floor(this.AP * 1.5);
-        console.log("this.AP after boost", this.AP);
-    }
+//     boostAP() {
+//         this.AP = Math.floor(this.AP * 1.5);
+//         console.log("this.AP after boost", this.AP);
+//     }
 
-    loseHP(opp) {
-        if (this === userChar) {
-            this.HP -= opp.CAP;
-        } else {
-            this.HP -= opp.AP;
-        }
-    }
+//     loseHP(opp) {
+//         if (this === userChar) {
+//             this.HP -= opp.CAP;
+//         } else {
+//             this.HP -= opp.AP;
+//         }
+//     }
 
-    reset() {
-        this.HP = this.baseHP;
-        this.AP = this.baseAP;
-        this.CAP = this.baseCAP;
-    }
-}
+//     reset() {
+//         this.HP = this.baseHP;
+//         this.AP = this.baseAP;
+//         this.CAP = this.baseCAP;
+//     }
+// }
 
 var slime = new Character("Slime", 20, 3, 4, "assets/media/images/slime.gif");
 slime.pushInCharArr();
